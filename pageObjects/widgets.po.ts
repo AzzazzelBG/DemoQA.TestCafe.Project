d45367eb856ae67d604cc6af71ea83a0bbad80d0
Aleksandar Drinkov <aleksandar.drinkov@sap.com>
2024-05-31 23:56:50 +0300

    Add widgets test

---
+++ pageObjects/widgets.po.ts
@@ -0,0 +1,9 @@
+import { Selector } from "testcafe";
+import { NavigationPageObject } from "../helpers/navigationBar.po";
+
+const navigationBar = new NavigationPageObject();
+
+export class WidgetsPageObject {
+
+
+}
