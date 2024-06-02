8cbb0d79f62d3efce1edb63552f7ed1c254bf10e
Alexander Drinkov <adrinkov@gmail.com>
2024-06-02 23:28:54 +0300

    Continue with widget tests

--- fixtures/widgetpage.fixture.ts
+++ fixtures/widgetpage.fixture.ts
@@ -1,16 +1,32 @@
 import { Selector } from "testcafe";
 import { NavigationPageObject } from "../helpers/navigationBar.po";
 import { HomePageObject } from "../pageObjects/homepage.po";
+import { WidgetsPageObject } from "../pageObjects/widgets.po";
 
 const homePage = new HomePageObject();
 const navigationBar = new NavigationPageObject();
+const widgetPage = new WidgetsPageObject();
 
-fixture('Widget page');
+fixture('Widget page')
+    .beforeEach(async t => await homePage.clickOnCard(t, 'Widgets'));
 
 test('Should navigate to Tabs menu', async t => {
-    await homePage.clickOnCard(t, 'Widgets');
-    await navigationBar.chooseSection(t, 'Tabs');
 
-    const tabsTitle = Selector('h1.text-center').withText('Tabs').exists;
+    await navigationBar.chooseSection(t, 'Tabs');
+    const tabsTitle = await widgetPage.titleExists(t, 'Tabs');
     await t.expect(tabsTitle).ok();
+});
+
+test('Should click Use tab', async t => {
+    await navigationBar.chooseSection(t, 'Tabs');
+    await widgetPage.clickTab(t, 'Use');
+    const isTabselected = await widgetPage.tabSelected('use');
+    await t.expect(isTabselected).eql('true');
+});
+
+test('Should go to Select Menu tab', async t => {
+    await navigationBar.chooseSection(t, 'Select Menu');
+    const selectMenuTitle = await widgetPage.titleExists(t, 'Select Menu');
+    await t.expect(selectMenuTitle).ok();
 })
+//TODO: Continue with the Slect Menu test
--- pageObjects/widgets.po.ts
+++ pageObjects/widgets.po.ts
@@ -5,5 +5,24 @@ const navigationBar = new NavigationPageObject();
 
 export class WidgetsPageObject {
 
+    private readonly _pageTitle = Selector('h1.text-center');
+    private readonly _selectDropGeneric = Selector('div.css-1hwfws3');
 
+    public async titleExists(t: TestController, titleText: string): Promise<boolean> {
+        return this._pageTitle.withText(`${titleText}`).exists;
+    }
+
+    public async clickTab(t: TestController, tabName: string): Promise<void> {
+        const tab = Selector(`a#demo-tab-${tabName.toLowerCase()}`);
+        await t.click(tab);
+    }
+
+    public async tabSelected(tabName: string): Promise<string | null> {
+        return Selector(`a#demo-tab-${tabName.toLowerCase()}`).getAttribute('aria-selected');
+    }
+
+    public async clickSelectValueDropdown(t: TestController): Promise<void> {
+        const selectValueDropdown = this._selectDropGeneric.nth(0);
+        await t.click(selectValueDropdown);
+    }
 }
